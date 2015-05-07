8022d870fb984110d9f774505501fcd8ed9012b2
leetschau <leetschau@gmail.com>
2015-05-07 18:28:01 +0800

    split chinese with english
    Signed-off-by: leetschau <leetschau@gmail.com>

--- segmentation-tests.js
+++ segmentation-tests.js
@@ -1,5 +1,10 @@
 // Write your tests here!
 // Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+Tinytest.add('splitBasic', function (test) {
+  test.equal(Seg.parse("May   7th"),  ["May", "7th"]);
+});
+
+Tinytest.add('splitChnEng', function (test) {
+  test.equal(Seg.parse("今天是2015年5月7日May 7th日期"),
+    ["今天是","2015","年","5","月","7","日","May","7th","日期"]);
 });
--- segmentation.coffee
+++ segmentation.coffee
@@ -1,3 +1,6 @@
 Seg =
   parse: (inp) ->
-    inp.split " "
+    # split by english, number and Chinese Words
+    rawParts = inp.split(/\b/)
+    part for part in rawParts when part.trim()
+    # split Chinese Words
