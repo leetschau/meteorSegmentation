f11cc2ab46f87e4acbdb069f1e903292356a6229
leetschau <leetschau@gmail.com>
2015-05-12 09:32:13 +0800

    bump up version to 0.1
    Signed-off-by: leetschau <leetschau@gmail.com>

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'leechau:segmentation',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'for chinese segmentation',
   // URL to the Git repository containing the source code for this package.
